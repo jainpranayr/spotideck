da7ec9cd4e08e2f6cbcc1108d6bf2b6512338eb1
Pranay Jain <jpranay688@gmail.com>
2022-08-03 02:38:12 +0530

    ✨ add Montserrat font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,11 @@
 import { ServerStyleSheet } from 'styled-components'
-import Document, { DocumentContext } from 'next/document'
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  NextScript,
+  Main,
+} from 'next/document'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -25,5 +31,28 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin=''
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
