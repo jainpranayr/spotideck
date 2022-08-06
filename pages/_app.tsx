7cab9e252dcb8a292da493c90c8c3c84b988f036
Pranay Jain <jpranay688@gmail.com>
2022-08-06 15:59:12 +0530

    💄 add progress bar on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,18 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { GlobalStyles } from '../styles'
 import { LogOutBtn } from '../components'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <GlobalStyles />
-      <LogOutBtn />
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <NextNProgress color='#1db954' height={4} />
+        <GlobalStyles />
+        <LogOutBtn />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
 
